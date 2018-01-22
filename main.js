4e726fc2184b214df0fec8528e09a719cdabf891
Manuel Rojas <mr.manuell@gmail.com>
2018-01-22 22:24:29 +0100

    added physics, collition not working

--- main.js
+++ main.js
@@ -13,39 +13,58 @@ window.onload = function() {
 
 //Function to update the frame of the canvas. This is where the animation happens
 function updateCanvas() {
-  Object.keys(keysPressed).forEach(function(direction) {
-    if (keysPressed[direction]) {
-      player.move(direction);
-    }
-  });
+  //Check for movement in controls for player 1
+  updatePlayerMovements(keysPressed, player, player2, accelerationActivated);
+
+  //Check for movement in controls for player 2
+  updatePlayerMovements(keysPressed2, player2, player, accelerationActivated2);
 
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  if (player.isColliding(obstacles.walls[0])) {
-    player.constrainWithPlayer2(obstacles.walls[0]);
-  }
-
   player.draw();
-  // if (player.isDead(obstacles.walls)) {
-  //   console.log("Collission detected");
-  //   //location.reload();
-  // }
-
-  obstacles.drawDummy();
+  player2.draw();
+  player.printResults(player2);
+  player2.printResults();
 
   requestAnimationFrame(updateCanvas);
 }
 
 //Input management
-//Definition of key to be used
+//Definition of key to be used: player
 var LEFT_KEY = 37;
 var RIGHT_KEY = 39;
 var DOWN_KEY = 40;
 var UP_KEY = 38;
+//Definition of key to be used: player2
+var LEFT_KEY_2 = 65;
+var RIGHT_KEY_2 = 68;
+var DOWN_KEY_2 = 83;
+var UP_KEY_2 = 87;
 
-//Holds the status of the pressed keys
+//Holds the status of the pressed keys: player 1
 var keysPressed = {
+  up: false,
+  down: false,
+  right: false,
+  left: false
+};
+//Holds the toggle if the acceleration was activated: player 1
+var accelerationActivated = {
+  up: false,
+  down: false,
+  right: false,
+  left: false
+};
+//Holds the status of the pressed keys: player 2
+var keysPressed2 = {
+  up: false,
+  down: false,
+  right: false,
+  left: false
+};
+//Holds the toggle if the acceleration was activated: player 1
+var accelerationActivated2 = {
   up: false,
   down: false,
   right: false,
@@ -55,6 +74,7 @@ var keysPressed = {
 //Handles the events triggered by the keys: case down
 document.onkeydown = function(event) {
   switch (event.keyCode) {
+    //Player 1
     case LEFT_KEY:
       keysPressed.left = true;
       break;
@@ -67,11 +87,25 @@ document.onkeydown = function(event) {
     case DOWN_KEY:
       keysPressed.down = true;
       break;
+    //Player 2
+    case LEFT_KEY_2:
+      keysPressed2.left = true;
+      break;
+    case RIGHT_KEY_2:
+      keysPressed2.right = true;
+      break;
+    case UP_KEY_2:
+      keysPressed2.up = true;
+      break;
+    case DOWN_KEY_2:
+      keysPressed2.down = true;
+      break;
   }
 };
 
 //Handles the events triggered by the keys: case up
 document.onkeyup = function(event) {
+  //Player 1
   switch (event.keyCode) {
     case LEFT_KEY:
       keysPressed.left = false;
@@ -84,6 +118,19 @@ document.onkeyup = function(event) {
       break;
     case DOWN_KEY:
       keysPressed.down = false;
+      break;
+    //Player 2
+    case LEFT_KEY_2:
+      keysPressed2.left = false;
+      break;
+    case RIGHT_KEY_2:
+      keysPressed2.right = false;
+      break;
+    case UP_KEY_2:
+      keysPressed2.up = false;
+      break;
+    case DOWN_KEY_2:
+      keysPressed2.down = false;
       break;
   }
 };
@@ -98,26 +145,55 @@ var obstacles = {
   r: 20,
   x: 200,
   y: 200,
-  mass: 100,
-
-  drawDummy: function(r) {
-    ctx.fillStyle = "blue";
-    //ctx.fillRect(this.x, this.y, this.width, this.height);
-    //context.arc(x,y,r,sAngle,eAngle,counterclockwise);
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-    ctx.closePath();
-    ctx.fill();
-  },
-
-  createDummy: function() {
-    this.walls.push({
-      x: this.x,
-      y: this.y,
-      r: this.r,
-      mass: this.mass
-    });
-  }
+  mass: 100
 };
 
-obstacles.createDummy();
+// function speedToggle(accelerationActivated, keysPressed, player) {
+//   if (accelerationActivated.up === true && keysPressed.up === false) {
+//     player.resetSpeedY();
+//     accelerationActivated.up = false;
+//   } else if (
+//     accelerationActivated.down === true &&
+//     keysPressed.down === false
+//   ) {
+//     player.resetSpeedY();
+//     accelerationActivated.down = false;
+//   } else if (
+//     accelerationActivated.left === true &&
+//     keysPressed.left === false
+//   ) {
+//     player.resetSpeedX();
+//     accelerationActivated.left = false;
+//   } else if (
+//     accelerationActivated.right === true &&
+//     keysPressed.right === false
+//   ) {
+//     player.resetSpeedX();
+//     accelerationActivated.right = false;
+//   }
+// }
+
+function updatePlayerMovements(
+  keysPressed,
+  player,
+  player2,
+  accelerationActivated
+) {
+  Object.keys(keysPressed).forEach(function(direction) {
+    //Check for movement in controls
+    if (keysPressed[direction]) {
+      player.accelerate(direction);
+      if (player.isColliding(player2)) {
+        player.setCollision(player2);
+      }
+      player.setMaxSpeed();
+      player.move(direction);
+      //Reset speeds if player stops from being accelerated
+      //Stores the info that the acceleration was activated in pressed direction
+      //accelerationActivated[direction] = true;
+      //Then checks if there is the key pressed and accelaration toggle match.
+      //If they don't match, it means the player stopped accelerating, so speed is reset.
+    }
+    //speedToggle(accelerationActivated, keysPressed, player);
+  });
+}
--- player.js
+++ player.js
@@ -4,9 +4,12 @@ var player = {
   r: 15,
   width: 50,
   height: 50,
-  speedX: 5,
-  speedY: 5,
-  mass: 10,
+  maxSpeed: 10,
+  acceleration: 0.01,
+  initialSpeed: 3,
+  speedX: 3,
+  speedY: 3,
+  mass: 100000,
 
   move: function(direction) {
     if (direction === "right") {
@@ -24,43 +27,220 @@ var player = {
 
   draw: function() {
     ctx.fillStyle = "red";
-    //ctx.fillRect(this.x, this.y, this.width, this.height);
-    //context.arc(x,y,r,sAngle,eAngle,counterclockwise);
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
     ctx.closePath();
     ctx.fill();
-    // ctx.drawImage(flappyImg, this.x, this.y, 50 * imgScale, 50);
   },
 
   isColliding: function(object) {
-    return (
-      Math.abs(this.x - object.x) < this.r + object.r &&
-      Math.abs(this.y - object.y) < this.r + object.r
-    );
-  },
-
-  // isDead: function(object) {
-  //   return object.some(this.isColliding.bind(this));
-  // },
-
-  //Function constrains the boundaries of player1 with any circular object.
-  constrainWithPlayer2: function(player2) {
     var x1 = this.x;
-    var x2 = obstacles.walls[0].x;
+    var x2 = object.x;
     var r1 = this.r;
-    var r2 = obstacles.walls[0].r;
+    var r2 = object.r;
     var y1 = this.y;
-    var y2 = obstacles.walls[0].y;
+    var y2 = object.y;
     var x = Math.abs(x2 - x1);
     var y = Math.abs(y2 - y1);
     var r = Math.sqrt(x ** 2 + y ** 2);
-    if (r <= r1 + r2) {
+    if (r < r1 + r2) {
+      console.log("collision1");
       //It takes the x and y coordinates of both players to find the angle of collision.
       //Then takes the distance between x2 and x1 and substracts it from x1.
       this.x -= (x2 - x1) / (r2 + r1) * r1;
       //Then takes the distance between y2 and y1 and substracts it from y1.
       this.y -= (y2 - y1) / (r2 + r1) * r1;
+      return true;
     }
+  },
+
+  //Accelerate
+  accelerate: function(direction) {
+    if (direction === "right") {
+      this.speedX += this.acceleration;
+    } else if (direction === "left") {
+      this.speedX += this.acceleration;
+    } else if (direction === "up") {
+      this.speedY += this.acceleration;
+    } else {
+      this.speedY += this.acceleration;
+    }
+  },
+
+  //Sets the speed to a maximum of maxSpeed
+  setMaxSpeed: function() {
+    this.speedX = Math.min(this.maxSpeed, this.speedX);
+    this.speedY = Math.min(this.maxSpeed, this.speedY);
+  },
+
+  resetSpeedX: function() {
+    this.speedX = this.initialSpeed;
+  },
+
+  resetSpeedY: function() {
+    this.speedY = this.initialSpeed;
+  },
+
+  setCollision: function(object) {
+    //Update speedX of player
+    this.speedX =
+      2 *
+        (this.mass * this.speedX + object.mass * object.speedX) /
+        (this.mass + object.mass) -
+      this.speedX;
+    console.log(this.speedX);
+    //Object speedX of player 2
+    object.speedX =
+      2 *
+        (this.mass * this.speedX + object.mass * object.speedX) /
+        (this.mass + object.mass) -
+      object.speedX;
+    //Update speedY of player
+    this.speedY =
+      2 *
+        (this.mass * this.speedY + object.mass * object.speedY) /
+        (this.mass + object.mass) -
+      this.speedY;
+    //Update speedY of player 2
+    object.speedY = this.speedY =
+      2 *
+        (this.mass * this.speedY + object.mass * object.speedY) /
+        (this.mass + object.mass) -
+      object.speedY;
+  },
+
+  printResults: function(object) {
+    //\n break line
+    var w = canvas.width - 200;
+    var h = 50;
+    var speedXAfterImpact1 =
+      2 *
+        (this.mass * this.speedX + object.mass * object.speedX) /
+        (this.mass + object.mass) -
+      this.speedX;
+    var speedYAfterImpact1 =
+      2 *
+        (this.mass * this.speedY + object.mass * object.speedY) /
+        (this.mass + object.mass) -
+      this.speedY;
+
+    var line1 = "Player1 speedX: " + this.speedX.toFixed(2);
+    var line2 = "Player1 speedY: " + this.speedY.toFixed(2);
+    var line3 = "SpeedX after impact: " + speedXAfterImpact1;
+    var line4 = "SpeedY after impact: " + speedYAfterImpact1;
+    ctx.font = "12px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText(line1, w, h);
+    ctx.fillText(line2, w, 1.3 * h);
+    ctx.fillText(line3, w, 1.6 * h);
+    ctx.fillText(line4, w, 1.9 * h);
   }
 };
+
+// Player.prototype.move = function(direction) {
+//   if (direction === "right") {
+//     this.x += this.speedX;
+//   } else if (direction === "left") {
+//     this.x -= this.speedX;
+//   } else if (direction === "up") {
+//     this.y -= this.speedY;
+//   } else {
+//     this.y += this.speedY;
+//   }
+//   this.x = Math.min(canvas.width - this.r, Math.max(0, this.x));
+//   this.y = Math.min(canvas.height - this.r, Math.max(0, this.y));
+// };
+// }
+
+// Player.prototype.draw = function() {
+//   ctx.fillStyle = "red";
+//   ctx.beginPath();
+//   ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+//   ctx.closePath();
+//   ctx.fill();
+// };
+
+// //Function constrains the boundaries of player1 with any circular object.
+// Player.prototype.isColliding = function(object) {
+//   var x1 = this.x;
+//   //var x2 = obstacles.walls[0].x;
+//   var x2 = object.x;
+//   var r1 = this.r;
+//   var r2 = object.r;
+//   var y1 = this.y;
+//   var y2 = object.y;
+//   var x = Math.abs(x2 - x1);
+//   var y = Math.abs(y2 - y1);
+//   var r = Math.sqrt(x ** 2 + y ** 2);
+//   if (r < r1 + r2) {
+//     console.log("collision");
+//     //It takes the x and y coordinates of both players to find the angle of collision.
+//     //Then takes the distance between x2 and x1 and substracts it from x1.
+//     this.x -= (x2 - x1) / (r2 + r1) * r1;
+//     //Then takes the distance between y2 and y1 and substracts it from y1.
+//     this.y -= (y2 - y1) / (r2 + r1) * r1;
+//   }
+// };
+
+// //Accelerate
+// Player.prototype.accelerate = function(direction) {
+//   if (direction === "right") {
+//     this.speedX += this.acceleration;
+//   } else if (direction === "left") {
+//     this.speedX += this.acceleration;
+//   } else if (direction === "up") {
+//     this.speedY += this.acceleration;
+//   } else {
+//     this.speedY += this.acceleration;
+//   }
+// };
+
+// //Sets the speed to a maximum of maxSpeed
+// Player.prototype.setMaxSpeed = function() {
+//   this.speedX = Math.min(this.maxSpeed, this.speedX);
+//   this.speedY = Math.min(this.maxSpeed, this.speedY);
+// };
+
+// Player.prototype.resetSpeedX = function() {
+//   this.speedX = this.initialSpeed;
+// };
+
+// Player.prototype.resetSpeedY = function() {
+//   this.speedY = this.initialSpeed;
+// };
+
+// Player.prototype.setCollision = function(object) {
+//   //Update speedX of player
+//   this.speedX =
+//     2 *
+//       (this.mass * this.speedX + object.mass * object.speedX) /
+//       (this.mass + object.mass) -
+//     this.speedX;
+//   //Object speedX of player 2
+//   object.speedX =
+//     2 *
+//       (this.mass * this.speedX + object.mass * object.speedX) /
+//       (this.mass + object.mass) -
+//     object.speedX;
+//   //Update speedY of player
+//   this.speedY =
+//     2 *
+//       (this.mass * this.speedY + object.mass * object.speedY) /
+//       (this.mass + object.mass) -
+//     this.speedY;
+//   //Update speedY of player 2
+//   object.speedY = this.speedY =
+//     2 *
+//       (this.mass * this.speedY + object.mass * object.speedY) /
+//       (this.mass + object.mass) -
+//     object.speedY;
+// };
+
+// Player.prototype.printResults = function(object) {
+//   var w = canvas.width - 200;
+//   var h = 50;
+//   //var line1 = "Player1 speedX: " + this.speedX.toFixed(2);
+//   ctx.font = "12px Arial";
+//   ctx.fillStyle = "white";
+//   //ctx.fillText(line1, w, h);
+// };
